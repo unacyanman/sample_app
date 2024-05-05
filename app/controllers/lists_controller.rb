022228699cb2cc31d1f0d913db2db4fcb5310f86
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-05 16:15:25 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,34 @@
+class ListsController < ApplicationController
+  
+  def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    list.save
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
+  end  
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end 
+  
+end
